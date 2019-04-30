dc8d023ddd7489c4bafb5db0e3f7315971cee979
Dmitry <Dimitry.C.Leyko@informa.com>
2019-04-30 17:20:15 +0300

    Sort error method pages

--- app/error/handler.py
+++ app/error/handler.py
@@ -4,13 +4,14 @@ from flask import render_template
 class ErrorPagesHandler:
 
     @staticmethod
+    def page_forbidden(error):
+        return render_template('errors/403.html', e=error), 403
+
+    @staticmethod
     def page_not_found(error):
-        return render_template('errors/404.html', error=error), 404
+        return render_template('errors/404.html', e=error), 404
 
     @staticmethod
     def internal_server_error(error):
-        return render_template('errors/500.html'), 500
+        return render_template('errors/500.html', e=error), 500
 
-    @staticmethod
-    def page_forbidden(error):
-        return render_template('errors/403.html'), 403
